55ca7d40f6baac11c5f576f3cfee079ec53df178
Samson <samblint@gmail.com>
2022-06-07 15:35:18 +0300

    commit tree 963a579f496bd8fdae6a1e994b87dc81278a087a

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1=31
+let num2=2
+let num3=24
+let num4=5
+let multiply = num1*num2
+let random=(Math.floor(Math.random() + 1));
+let mod=(num3%num4);
+let max =Math.max(1,2,3,10,20)
 
-// Write your code here
